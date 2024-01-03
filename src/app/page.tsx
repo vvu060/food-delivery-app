b7ff6e944013b9c1c9b0541167f03a6c6b68845b
vvu060 <vvu060@gmail.com>
2024-01-03 21:15:17 +0530

    Add header and hero components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,11 @@
+import Header from '@/components/layout/Header';
+import Hero from '@/components/layout/Hero';
+
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
 }
