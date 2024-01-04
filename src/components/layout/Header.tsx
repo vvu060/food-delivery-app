1c175a385709b16a65872f81a58d0007b24f118e
vvu060 <vvu060@gmail.com>
2024-01-04 19:09:54 +0530

    add paths

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -4,10 +4,10 @@ const Header = () => {
   return (
     <header className='flex items-center justify-between'>
       <nav className='flex items-center gap-8 text-gray-500 font-semibold'>
-        <Link className='text-primary font-semibold text-2xl' href=''>
+        <Link className='text-primary font-semibold text-2xl' href='/'>
           ST PIZZA
         </Link>
-        <Link href=''>Home</Link>
+        <Link href='/'>Home</Link>
         <Link href=''>Menu</Link>
         <Link href=''>About</Link>
         <Link href=''>Contact</Link>
