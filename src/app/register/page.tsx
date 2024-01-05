f8a1859662a1e81e8809046d29a0ef63b2e777c3
vvu060 <vvu060@gmail.com>
2024-01-05 09:47:01 +0530

    Add user registration functionality

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,14 +1,74 @@
+'use client';
 import Image from 'next/image';
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 
 const RegisterPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [creatingUser, setCreatingUser] = useState(false);
+  const [userCreated, setUserCreated] = useState(false);
+  const [error, setError] = useState(false);
+
+  const handleFormSubmit = async (e: any) => {
+    e.preventDefault();
+    setCreatingUser(true);
+    setError(false);
+    setUserCreated(false);
+
+    const response = await fetch('/api/register', {
+      method: 'POST',
+      body: JSON.stringify({ email, password }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      setCreatingUser(false);
+      setUserCreated(true);
+      setEmail('');
+      setPassword('');
+    } else {
+      setCreatingUser(false);
+      setError(true);
+    }
+  };
+
   return (
     <section className='mt-8'>
       <h1 className='text-center text-primary text-4xl mb-4'>Register</h1>
-      <form className='block max-w-xs mx-auto'>
-        <input type='email' name='' id='' placeholder='email' />
-        <input type='password' name='' id='' placeholder='password' />
-        <button type='submit'>Register</button>
+      {userCreated && (
+        <div className='my-4 text-center'>
+          User Created <br /> Now you can{' '}
+          <Link href='/login' className='underline'>
+            login &raquo
+          </Link>{' '}
+        </div>
+      )}
+      {error && (
+        <div className='my-4 text-center'>
+          An error occurred <br /> Please try again later
+        </div>
+      )}
+      <form className='block max-w-xs mx-auto' onSubmit={handleFormSubmit}>
+        <input
+          type='email'
+          value={email}
+          disabled={creatingUser}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder='email'
+        />
+        <input
+          type='password'
+          value={password}
+          disabled={creatingUser}
+          onChange={(e) => setPassword(e.target.value)}
+          placeholder='password'
+        />
+        <button type='submit' disabled={creatingUser}>
+          Register
+        </button>
         <div className='my-4 text-center text-gray-500'>
           or login with provider
         </div>
@@ -16,6 +76,12 @@ const RegisterPage = () => {
           <Image src='/google.png' alt='google' width='24' height='24' />
           Login with google
         </button>
+        <div className='text-center my-4 text-gray-500 border-t pt-4'>
+          Existing user?{' '}
+          <Link className='underline' href='/login'>
+            Login here &raquo;
+          </Link>
+        </div>
       </form>
     </section>
   );
