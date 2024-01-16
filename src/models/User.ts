b794109ad672fc9b22a13fc3791cefa8262a7b3d
vvu060 <vvu060@gmail.com>
2024-01-16 11:19:16 +0530

    Add name field to User model

--- src/models/User.ts
+++ src/models/User.ts
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt';
 
 const UserSchema = new Schema(
   {
+    name: { type: String },
     email: { type: String, required: true, unique: true },
     password: {
       type: String,
